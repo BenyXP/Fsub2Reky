a7cf7fa2db10ce18eb97c43b040fbff7e6bf86fa
BenyXP <103335260+BenyXP@users.noreply.github.com>
2022-07-28 02:10:48 +0700

    Update must_join.py

--- ForceSubscribeBot/must_join.py
+++ ForceSubscribeBot/must_join.py
@@ -25,8 +25,24 @@ async def must_join_channel(bot: Client, msg: Message):
                         [InlineKeyboardButton("✨ Join Channel ✨", url=link)]
                     ])
                 )
+try:
+            await bot.get_chat_member(MUST_JOIN, msg.from_user.id)
+        except UserNotParticipant:
+            if MUST_JOIN.isalpha():
+                link = "https://t.me/" + MUST_JOIN
+            else:
+                chat_info = await bot.get_chat(MUST_JOIN)
+                link = chat_info.invite_link
+            try:
+                await msg.reply(
+                    f"You must join [this channel]({link}) to use me. After joining try again !",
+                    disable_web_page_preview=True,
+                    reply_markup=InlineKeyboardMarkup([
+                        [InlineKeyboardButton("✨ Join Channel ✨", url=link2)]
+                    ])
+                )
                 await msg.stop_propagation()
             except ChatWriteForbidden:
                 pass
     except ChatAdminRequired:
-        print(f"I'm not admin in the MUST_JOIN chat : {MUST_JOIN} !")
+        print(f"Saya bukan admin di obrolan harus join : {MUST_JOIN} !")
